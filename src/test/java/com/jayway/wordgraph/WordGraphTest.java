b277b524de34671059dad010759630ee7f5edb27
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2011-04-27 16:46:53 +0200

    Now using constants for numbering.

--- src/test/java/com/jayway/wordgraph/WordGraphTest.java
+++ src/test/java/com/jayway/wordgraph/WordGraphTest.java
@@ -19,38 +19,38 @@ public class WordGraphTest {
     public void dummyTest() {
     }
 
-    // @BEGIN_VERSION 1
+    // @BEGIN_VERSION GATHER_WORDS_WHITESPACE
     // (describe gather-words
     //   (it "splits words on whitespace"
     //     (= ["mary" "had" "a" "little" "lamb"]
     //        (gather-words "   mary had a\tlittle\n   lamb    ")))
-    // @BEGIN_VERSION 2
+    // @BEGIN_VERSION GATHER_WORDS_PUNCTUATION
     //   (it "removes punctuation"
     //     (= ["mary" "had" "a" "little" "lamb"]
     //        (gather-words "., mary, had... a little; lamb!")))
-    // @END_VERSION 2
-    // @BEGIN_VERSION 3
+    // @END_VERSION GATHER_WORDS_PUNCTUATION
+    // @BEGIN_VERSION GATHER_WORDS_LOWERCASE
     //   (it "converts words to lower case"
     //     (= ["mary" "had" "a" "little" "lamb"]
     //        (gather-words "., MaRy, hAd... A liTTle; lAmb!"))))
-    // @END_VERSION 3
+    // @END_VERSION GATHER_WORDS_LOWERCASE
     @Test
     public void testGatherWords() {
         String[] expected = new String[] {"mary", "had", "a", "little", "lamb"};
         assertThatCollection(WordGraph.gatherWords("   mary had a\tlittle\n   lamb    "), is(expected));
 
-        // @BEGIN_VERSION 2
+        // @BEGIN_VERSION GATHER_WORDS_PUNCTUATION
         assertThatCollection(WordGraph.gatherWords("., mary, had... a little; lamb!"), is(expected));
-        // @END_VERSION 2
+        // @END_VERSION GATHER_WORDS_PUNCTUATION
 
-        // @BEGIN_VERSION 3
+        // @BEGIN_VERSION GATHER_WORDS_LOWERCASE
         assertThatCollection(WordGraph.gatherWords("., MaRy, hAd... A liTTle; lAmb!"), is(expected));
-        // @END_VERSION 3
+        // @END_VERSION GATHER_WORDS_LOWERCASE
     }
 
-    // @END_VERSION 1
+    // @END_VERSION GATHER_WORDS_WHITESPACE
 
-    // @BEGIN_VERSION 4
+    // @BEGIN_VERSION COUNT_WORDS
     // (describe count-words
     //   (it "counts words into a map"
     //     (= {"mary" 2 "why" 3 } (count-words ["why" "mary" "why" "mary" "why"]))))
@@ -64,9 +64,9 @@ public class WordGraphTest {
         assertThat(WordGraph.countWords(Arrays.asList("why", "mary", "why", "mary", "why")), is(expected));
     }
 
-    // @END_VERSION 4
+    // @END_VERSION COUNT_WORDS
 
-    // @BEGIN_VERSION 5
+    // @BEGIN_VERSION SORT_COUNTED_WORDS
     // (describe sort-counted-words
     //   (it "sorts and returns a list of word/count pairs"
     //     (= [["a" 1] ["c" 2] ["b" 3]] (sort-counted-words {"b" 3 "c" 2 "a" 1}))))
@@ -84,9 +84,9 @@ public class WordGraphTest {
             .build();
         assertThat(WordGraph.sortCountedWords(input), is(expected));
     }
-    // @END_VERSION 5
+    // @END_VERSION SORT_COUNTED_WORDS
 
-    // @BEGIN_VERSION 6
+    // @BEGIN_VERSION REPEAT_STR
     // (describe repeat-str
     //   (it "returns the empty string if count is zero"
     //     (= "" (repeat-str "*" 0)))
@@ -97,9 +97,9 @@ public class WordGraphTest {
         assertThat(WordGraph.repeatStr("*", 0), is(""));
         assertThat(WordGraph.repeatStr("x", 5), is("xxxxx"));
     }
-    // @END_VERSION 6
+    // @END_VERSION REPEAT_STR
 
-    // @BEGIN_VERSION 7
+    // @BEGIN_VERSION HISTOGRAM_ENTRY
     // (describe histogram-entry
     //   (it "can generate a single histogram entry"
     //     (= "betty   ######" (histogram-entry ["betty" 6] 7))))
@@ -107,9 +107,9 @@ public class WordGraphTest {
     public void testHistogramEntry() {
         assertThat(WordGraph.histogramEntry(singletonMap("betty", 6), 7), is("betty   ######"));
     }
-    // @END_VERSION 7
+    // @END_VERSION HISTOGRAM_ENTRY
 
-    // @BEGIN_VERSION 8
+    // @BEGIN_VERSION HISTOGRAM
     // (describe histogram
     //   (it "can generate a histogram from word counts"
     //     (= "mary ##\nwhy  ###\n" (histogram [["mary" 2] ["why" 3]]))))
@@ -121,7 +121,7 @@ public class WordGraphTest {
                 .build();
         assertThat(WordGraph.histogram(input), is("mary ##\nwhy  ###\n"));
     }
-    // @END_VERSION 8
+    // @END_VERSION HISTOGRAM
 
     // had trouble getting assertThat to compare a transformed collection with a list or array
     private void assertThatCollection(Collection<String> actual, Matcher<String[]> matcher) {
